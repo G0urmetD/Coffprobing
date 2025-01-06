8b449b0ab9223609044d29944948e12172ca7b79
G0urmet <60777262+G0urmetD@users.noreply.github.com>
2025-01-06 14:25:48 +0100

    Update coffprobing.py

--- coffprobing.py
+++ coffprobing.py
@@ -9,7 +9,7 @@ from colorama import Fore, Style
 from pathlib import Path
 
 # Constants
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 BANNER = f"""
 
    ______      ________                 __    _            
